5c40d3d5679f54163f583887399c28e21077d5a5
Igor <machold.igor@gmail.com>
2022-07-15 01:07:43 +0200

    new version

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login/Login";
 import Main from "./components/Main/Main";
-import Register from "./components/Register/Register";
 import Secure from "./components/Secure/Secure";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
           <Route exact path="/" component={Main} />
           <Route exact path="/secure" component={Secure} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,59 +2,91 @@ import React, { useState } from "react";
 import "./login.css";
 
 export default function Login() {
-  // Storing the username and password in an array
-  const data = [{ username: "Igor" }, { password: "123456wW" }];
-
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  // Storing the username and password in an array
+  const database = [
+    { username: "Igor1", password: "12345qQ" },
+    { username: "Igor2", password: "12345wW" },
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password",
+  };
 
   console.log("username is", username);
   console.log("password is", password);
 
+  const renderErrorMessage = (name) => {
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // I was trying to store the data in localstorage, but apparently i am missing something
+    const { uname, pass } = document.forms[0];
 
-    if (
-      username.current.value === "Igor" &&
-      password.current.value === "123456wW"
-    ) {
-      localStorage.setItem("UsernameData", "Igor");
-      localStorage.setItem("PasswordData", "123456wW");
+    //Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    //Compare user info
+
+    if (userData) {
+      if (userData.password !== pass.value) {
+        //Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      //Username not found
+
+      setErrorMessages({ name: "uname", message: errors.uname });
     }
   };
   return (
     <div className="login-container">
       <div className="login-form">
-        <form action="">
-          <p className="main-text">Login to your Account</p>
-          <div className="login-input">
-            <label htmlFor="text">Username</label>
-            <input
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              type="text"
-              placeholder="username"
-              pattern="^[A-Za-z]\\w{5, 29}$"
-              required
-            />
-          </div>
-          <div className="password-input">
-            <label htmlFor="">Password</label>
-            <input
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              type="text"
-              placeholder="password"
-              pattern="^(?=.*[0-9])(?=.*[a-zA-Z])(?=\S+$).{6,20}$"
-              required
-            />
-          </div>
-          <button onSubmit={handleSubmit} className="login-button">
-            Login
-          </button>
-        </form>
+        <p className="main-text">Login to your Account</p>
+        {isSubmitted ? (
+          <div className="success">User is successfully logged in</div>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <div className="login-input">
+              <label htmlFor="text">Username</label>
+              <input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                name="uname"
+                type="text"
+                placeholder="username"
+                pattern="^[\w\s-]{5,9}$"
+                required
+              />
+              {renderErrorMessage("uname")}
+            </div>
+            <div className="password-input">
+              <label htmlFor="">Password</label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                name="pass"
+                type="text"
+                placeholder="password"
+                pattern="^(?=.*[0-9])(?=.*[a-zA-Z])(?=\S+$).{6,20}$"
+                required
+              />
+              {renderErrorMessage("pass")}
+            </div>
+            <button className="login-button">Login</button>
+          </form>
+        )}
       </div>
     </div>
   );
